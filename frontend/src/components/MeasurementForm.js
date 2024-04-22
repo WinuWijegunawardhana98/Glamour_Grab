c768a83051d8fb71cf83fb548976e17dfaa5f38b
Winu <winodyawijegunawardhana@gmail.com>
2024-04-22 22:56:00 +0530

    frontend implementation

---
+++ frontend/src/components/MeasurementForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+
+const MeasurementForm = ({ onSubmit }) => {
+  return (
+    <Formik
+      initialValues={{ height: '', weight: '', chest: '', waist: '', hips: '' }}
+      onSubmit={(values) => onSubmit(values)}
+    >
+      {({ handleChange, handleSubmit, values }) => (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="height">
+            <Form.Label>Height (cm)</Form.Label>
+            <Form.Control type="number" name="height" value={values.height} onChange={handleChange} />
+          </Form.Group>
+          {/* Add more Form.Group components for other measurements (weight, chest, waist, hips) */}
+          <Button variant="primary" type="submit">
+            Adjust Size
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default MeasurementForm;
---
+++ frontend/src/components/ModelDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ModelDisplay = ({ measurements }) => {
+  // Logic to display the model based on measurements
+  return (
+    <div>
+      {/* Add your model display logic here */}
+      <p>Model Display</p>
+    </div>
+  );
+};
+
+export default ModelDisplay;
