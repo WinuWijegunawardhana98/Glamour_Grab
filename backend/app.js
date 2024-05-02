3cb4f06df9d79af9bc039f16d062b72fe2edd346
Winu <winodyawijegunawardhana@gmail.com>
2024-05-02 09:59:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,8 @@ const bodyParser= require("body-parser");
 const dotenv =require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
 const male = require("./models/male.model");
-const FittingRoute = require("./routes/routes");
+const FittingRoute = require("./routes/routes")
+;
 //app
 const app=express();
 
