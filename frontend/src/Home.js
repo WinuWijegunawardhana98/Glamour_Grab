16e5d9c8af6c4d784e09bf5590e877d2536f1c00
Winu <winodyawijegunawardhana@gmail.com>
2024-05-08 01:32:15 +0530

    Header And Footer Creation

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my app!</h1>
+      {/* Add your main page content here */}
+    </div>
+  );
+};
+
+export default Home;
