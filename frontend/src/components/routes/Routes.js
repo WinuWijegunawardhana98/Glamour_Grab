ffab2fbcf34d915f052bd937667c39fc1d5a7ff1
IT21171406 <99183050+IT21171406@users.noreply.github.com>
2024-05-07 21:39:41 +0530

    Update Routes.js

--- frontend/src/components/routes/Routes.js
+++ frontend/src/components/routes/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "../App";
 import MeasurementForm from "../component/MeasurementForm";
 import ModelDisplay from "../ModelDisplay";
+import OrderStatusPage from "../OrderStatusPage";
+
 
 const Route = () => {
     return (
@@ -10,6 +12,7 @@ const Route = () => {
         <Routes> {/* Use Routes instead of Switch */}
           <Route exact path="/" element={<App />} /> {/* Specify element prop */}
           <Route path="/measurement-form" element={<MeasurementForm />} /> {/* Specify element prop */}
+          <Route path ="/oder-status" element={<OrderStatusPage/>}/>
           {/* Add additional routes as needed */}
         </Routes>
       </Router>
